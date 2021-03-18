a57513b5eaf257d24b6a4183f6f9ef526695ae40
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-18 17:29:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,12 @@
 # code your #position_taken? method here!
+#def position_taken?(board, index)
+#  board[index] != " "
+#end
+
 def position_taken?(board, index)
-  board[index] != " "
+  if (board[index] == " ")  (board[index] == "")  (board[index] ==nil)
+    return false
+  else
+    return true
+  end
 end
