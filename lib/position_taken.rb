d5623f9188fac4b0d860a9cde5546cce0956352e
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-18 17:22:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -14,3 +14,11 @@ end
 def position_taken?(board, index)
   board[index] != " "
 end
+
+def valid_move?(board, index)
+    if index.between?(0, 8) && !position_taken?(board, index)
+      true
+    else
+      false
+    end
+end
